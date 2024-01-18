e0d0484151e2a06ccace9e3e3ed8732023169286
nashwaatika13 <nashwaatika7@gmail.com>
2024-01-18 09:54:07 +0700

    first commit

---
+++ lib/models/item_model.dart
@@ -0,0 +1,9 @@
+class Item {
+  String? title;
+  String? category;
+  String? thumb_url;
+  String? location;
+  double? price;
+
+  Item(this.title, this.category, this.location, this.price, this.thumb_url);
+}
